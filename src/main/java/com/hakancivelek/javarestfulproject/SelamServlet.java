ac30b801868e5a19477488ce33a918b1d8998e19
Hakan CİVELEK <hakancivelek5461@gmail.com>
2025-03-04 20:45:59 +0300

    initial commit

---
+++ src/main/java/com/hakancivelek/javarestfulproject/SelamServlet.java
@@ -0,0 +1,26 @@
+package com.hakancivelek.javarestfulproject;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/selam-servlet")
+public class SelamServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Selam Servlet!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
